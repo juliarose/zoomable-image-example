ccc39b949bc28df5ad3b17456a5c81ad2c13ad89
juliarose <julialunarose@gmail.com>
2026-02-06 06:21:58 -0500

    Uncompressed tiles

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ window.addEventListener("DOMContentLoaded", () => {
 	OpenSeadragon({
 		id: "openseadragon1",
 		prefixUrl,
-		tileSources: "tiles/IMG_7881.dzi",
+		tileSources: "IMG_7881.dzi",
 		minZoomLevel: 0.5,
 		maxZoomLevel: 20,
 		maxZoomPixelRatio: 10,
