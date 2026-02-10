75a435353aaecdd853004db315132b1824fb593a
juliarose <julialunarose@gmail.com>
2026-02-10 12:34:47 -0500

    Update main.js to disable tiling, replace tabs with spaces

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
 import OpenSeadragon from "openseadragon";
 
 const prefixUrl = new URL(
-	"openseadragon/build/openseadragon/images/",
-	import.meta.url
+    "openseadragon/build/openseadragon/images/",
+    import.meta.url
 ).href;
 
 window.addEventListener("DOMContentLoaded", () => {
-	OpenSeadragon({
-		id: "openseadragon1",
-		prefixUrl,
-		tileSources: [{
-			type:"zoomifytileservice",
-			width: 2000,
-			height: 1332,
-			tilesUrl: "image/",
-		}],
-		wrapHorizontal: true,
-		wrapVertical: false,
-		maxZoomLevel: 100,
-		maxZoomPixelRatio: 1,
-		minZoomLevel: 0,
-		showNavigator: false,
-		showNavigationControl: false,
-		showZoomControl: false,
-		showHomeControl: false,
-		showFullPageControl: false,
-	});
+    OpenSeadragon({
+        id: "openseadragon1",
+        prefixUrl,
+        tileSources: [{
+            type:"zoomifytileservice",
+            width: 2000,
+            height: 1332,
+            tilesUrl: "image/",
+        }],
+        // uncomment for wrapping panoramas
+        // wrapHorizontal: true,
+        // wrapVertical: false,
+        maxZoomLevel: 100,
+        maxZoomPixelRatio: 1,
+        minZoomLevel: 0,
+        showNavigator: false,
+        showNavigationControl: false,
+        showZoomControl: false,
+        showHomeControl: false,
+        showFullPageControl: false,
+    });
 });
